441efa7280ff703a3f99d742f5cd3d10d2b5cff0
Jermaine <jermainenee@yeah.net>
2022-07-07 14:59:33 +0800

    Authentication and register

--- trip-web-server/trip-web-api/src/main/java/cn/zm/trip/restful/AuthController.java
+++ trip-web-server/trip-web-api/src/main/java/cn/zm/trip/restful/AuthController.java
@@ -1,14 +1,43 @@
 package cn.zm.trip.restful;
 
+import cn.zm.common.base.ResResult;
+import cn.zm.common.enums.ResEnum;
+import cn.zm.common.utils.AssertUtil;
+import cn.zm.trip.entity.ViewUserAccount;
+import cn.zm.trip.entity.dto.ViewUserAccountDTO;
+import cn.zm.trip.entity.vo.ViewUserAccountVO;
+import cn.zm.trip.service.IViewUserAccountService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 
+@Transactional(rollbackFor = Exception.class)
 @RestController
 @RequestMapping("auth")
 @Api(tags = "权限认证")
 public class AuthController {
-  // @GetMapping("/login")
-  // @ApiOperation(value = "下载文件")
+  @Resource
+  private IViewUserAccountService userAccountService;
+
+
+  @PostMapping("/login")
+  @ApiOperation("登陆")
+  public ResResult<ViewUserAccountVO> login(@RequestBody ViewUserAccountDTO dto) {
+    ViewUserAccount one = userAccountService.getOne(new QueryWrapper<>(dto.convert()));
+    AssertUtil.assertNonNull(one, ResEnum.INVALID_PARAMS);
+    return ResResult.succ(one.convert());
+  }
+
+  @PostMapping("/register")
+  @ApiOperation("注册")
+  public ResResult register(@RequestBody ViewUserAccountDTO dto) {
+    userAccountService.register(dto);
+    return ResResult.succ();
+  }
+
 }
