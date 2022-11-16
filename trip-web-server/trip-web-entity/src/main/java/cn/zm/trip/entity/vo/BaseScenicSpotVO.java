6a03dda945aacb0ac2ffda00ee53c0f9df6ab8f1
Jermaine Nee <jermainenee@yeah.net>
2022-11-16 10:04:19 +0800

    Update BaseScenicSpotVO.java

--- trip-web-server/trip-web-entity/src/main/java/cn/zm/trip/entity/vo/BaseScenicSpotVO.java
+++ trip-web-server/trip-web-entity/src/main/java/cn/zm/trip/entity/vo/BaseScenicSpotVO.java
@@ -42,8 +42,8 @@ public class BaseScenicSpotVO {
     @ApiModelProperty(value = "景点开放时间范围")
     private String openRangeTime;
     @ApiModelProperty(value = "景点信息发布时间")
-    @TableField("creatTime")
-    private LocalDateTime creatTime;
+    @TableField("createTime")
+    private LocalDateTime createTime;
     @ApiModelProperty(value = "景点描述内容")
     private String content;
 }
